7bfe4e43bf083c180869a7c67ca31cd28e437b63
Akib7861235 <sonusheak756@gmail.com>
2023-08-14 12:14:59 +0530

    basic  task commit

---
+++ TestProject/src/TestPackage/Testcase1.java
@@ -0,0 +1,11 @@
+package TestPackage;
+
+public class Testcase1 {
+
+	public static void main(String[] args) {
+		System.out.println("enter username");
+		System.out.println("enter password");
+		System.out.println("click on login button");
+	}
+
+}
