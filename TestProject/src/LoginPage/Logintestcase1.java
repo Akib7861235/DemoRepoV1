9d357cbc06bfd083d8b35cebc365e96d48aeac35
Akib7861235 <sonusheak756@gmail.com>
2023-08-14 13:23:08 +0530

    LoginFeatureNew

---
+++ TestProject/src/LoginPage/Logintestcase1.java
@@ -0,0 +1,9 @@
+package LoginPage;
+
+public class Logintestcase1 {
+
+	public static void main(String[] args) {
+		System.out.println("this is login test case");
+	}
+
+}
--- TestProject/src/TestPackage/Testcase1.java
+++ TestProject/src/TestPackage/Testcase1.java
@@ -5,7 +5,7 @@ public class Testcase1 {
 	public static void main(String[] args) {
 		System.out.println("enter username");
 		System.out.println("enter password");
-		System.out.println("click on login button");
+		System.out.println("click on login button and navigate to home page of the application");
 	}
 
 }
